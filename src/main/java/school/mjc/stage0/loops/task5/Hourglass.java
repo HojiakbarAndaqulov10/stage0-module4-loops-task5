3ff9d22dbfe8134c7e9d457705f4ed2b42df25b2
Your Name <you@example.com>
2023-12-24 22:43:25 +0500

    thank you lesson

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i <= height / 2) {
+                    if (j >= i && j < height - i) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    if (j >= height - i - 1 && j < i + 1) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
